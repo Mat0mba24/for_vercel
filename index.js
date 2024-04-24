1a949c310b18217c4690bf76a2a9e9a4e0334b3f
Mat0mba24 <mat0mba@mail.ru>
2024-04-24 21:59:32 +0300

    change code

--- index.js
+++ index.js
@@ -4,7 +4,16 @@ const app = express();
 
 app.get('/login', (req, res) => res.send('mat0mba'));
 
-app.route('/deg/:n1/:n2').get(r => r.res.send(Number(r.params.n1) ** Number(r.params.n2)));
+app.get('/deg/:n1/:n2', (req, res) => {
+	const { n1, n2 } = req.params;
+
+	if (isNaN(n1) || isNaN(n2)) {
+		return res.status(400).send('Both n1 and n2 must be numbers');
+	}
+
+	res.send(`${Number(n1) ** Number(n2)}`);
+});
+
 
 const port = process.env.PORT || 3000;
 
